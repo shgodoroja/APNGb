e4477dec463009f0aa8dba6798ee5484070a3e71
Stefan Godoroja <stefan@smallte.ch>
2016-09-17 23:26:38 +0300

    Added Xcode project

---
+++ APNGb/APNGb/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  APNGb
+//
+//  Created by Stefan Godoroja on 9/17/16.
+//  Copyright © 2016 Godoroja Stefan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ APNGb/APNGb/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  APNGb
+//
+//  Created by Stefan Godoroja on 9/17/16.
+//  Copyright © 2016 Godoroja Stefan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
