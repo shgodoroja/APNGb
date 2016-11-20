061a72386782c4e3da375eb111cf6482e6e6481a
Stefan Godoroja <stefan@smallte.ch>
2016-11-20 14:52:19 +0100

    Added ConvertGifViewController and designed it’s view.

---
+++ APNGb/APNGb/ViewControllers/ConvertGifViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConvertGifViewController.swift
+//  APNGb
+//
+//  Created by Stefan Godoroja on 11/20/16.
+//  Copyright © 2016 Godoroja Stefan. All rights reserved.
+//
+
+import Cocoa
+
+class ConvertGifViewController: NSViewController {
+    
+}
