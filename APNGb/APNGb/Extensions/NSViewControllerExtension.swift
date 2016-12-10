f8fb0a201bffe98f04dbb5afaca4459431536cbe
Stefan Godoroja <stefan@smallte.ch>
2016-12-10 21:26:23 +0100

    Missed a file for previous commit.

--- APNGb/APNGb/Extensions/NSViewControllerExtension.swift
+++ APNGb/APNGb/Extensions/NSViewControllerExtension.swift
@@ -10,26 +10,47 @@ import Cocoa
 
 extension NSViewController {
     
-    /// Removes all child view controllers together with their views
+    /// Removes all child view controllers together with associated views.
     func removeChildViewControllers() {
         
         for childViewController in self.childViewControllers {
             childViewController.view.removeFromSuperview()
             childViewController.removeFromParentViewController()
         }
-        
     }
     
-    func showChildViewController(withIdentifier identifier: String) {
+    /// Removes all child view controllers together with associated views, ignoring
+    /// view controllers from the parameter.
+    /// - Parameter viewControllers: view controllers which won't be removed from 
+    /// `childviewcontrollers` stack.
+    func removeChildViewControllersExcept(viewControllers: [NSViewController]) {
+        
+        for childViewController in self.childViewControllers {
+            
+            if viewControllers.contains(childViewController) == false {
+                
+                childViewController.view.removeFromSuperview()
+                childViewController.removeFromParentViewController()
+            }
+        }
+    }
+    
+    /// Adds child view controller to `childviewcontrollers` stack and
+    /// it's view to current view controller's associated view.
+    /// - Parameter identifier: Storyboard identifier of the child view controller.
+    /// - Returns: View controller identified by storyboard identifier or `nil` if
+    /// no view controller was associated with that identifier.
+    func showChildViewController(withIdentifier identifier: String) -> NSViewController? {
         let loadedController = storyboard?.instantiateController(withIdentifier: identifier)
         
         if loadedController is NSViewController {
             let childViewController = loadedController as! NSViewController
-            
-            self.removeChildViewControllers()
-            childViewController.view.frame = self.view.frame
             self.addChildViewController(childViewController)
+            childViewController.view.translatesAutoresizingMaskIntoConstraints = false
             self.view.addSubview(childViewController.view)
+            return childViewController
+        } else {
+            return nil
         }
     }
 }
