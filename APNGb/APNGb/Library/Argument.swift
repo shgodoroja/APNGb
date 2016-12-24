18adb8ad2c15028bdc89c6576a2c5434489b826c
Stefan Godoroja <stefan@smallte.ch>
2016-12-24 22:54:19 +0100

    Missed file in previous commit.

---
+++ APNGb/APNGb/Library/Argument.swift
@@ -0,0 +1,13 @@
+//
+//  Argument.swift
+//  APNGb
+//
+//  Created by Stefan Godoroja on 12/24/16.
+//  Copyright © 2016 Godoroja Stefan. All rights reserved.
+//
+
+import Cocoa
+
+class Argument: NSObject {
+
+}
